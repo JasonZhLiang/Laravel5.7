6e853319637e7db5e89ae8fa679794a598d21970
JasonZhLiang <Jasonzh.liang@gmail.com>
2019-05-08 09:29:02 -0400

    init code

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>laravelsite-contactpage</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 3em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Contact Form</h1>
+
+</body>
+</html>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>laravelsite-homepage</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 3em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Home page</h1>
+
+<p>
+    <a href="/contact">Contact</a> us to learn more.
+</p>
+
+</body>
+</html>
