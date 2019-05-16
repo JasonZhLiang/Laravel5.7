bbfb04ae0fde4e52d56493a3c1e55e93a6c99ef3
JasonZhLiang <Jasonzh.liang@gmail.com>
2019-05-16 13:00:40 -0400

    eloquent model added

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('titile', 'Laravel project')</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 3em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+</html>
