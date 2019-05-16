0058aa893f93161e82a851c032d5b30d8408bc05
JasonZhLiang <Jasonzh.liang@gmail.com>
2019-05-16 13:03:14 -0400

    config and new model generated by artisan:

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('titile', 'Laravel project')</title>
+    <title>@yield('title', 'Laravel project')</title>
     <style>
         header {
             background: #e3e3e3;
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title','projects homepage')
+
+@section('content')
+
+    <h1>Projects</h1>
+
+    <ul>
+        @foreach ($projects as $key=>$project)
+            <li>{{$project->title}} [{{$key}}] ({{$project->description}}) {{$project->created_at}}</li>
+        @endforeach
+    </ul>
+
+@endsection
