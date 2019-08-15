2cb75a70efcc394ac676d4c0a4c2b5e8fe784176
JasonZhLiang <Jasonzh.liang@gmail.com>
2019-08-15 11:47:10 -0400

    add show blade file

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,13 +3,13 @@
 @section('title','projects homepage')
 
 @section('content')
-
+<div class="content">
     <h1 class="title">Projects</h1>
 
-    <ul>
+    <ul class="">
         @foreach ($projects as $key=>$project)
             <li>{{$project->title}} [{{$key}}] ({{$project->description}}) {{$project->created_at}}</li>
         @endforeach
     </ul>
-
+</div>
 @endsection
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title','show project')
+
+@section('content')
+
+    <h1 class="title">{{ $project->title }} </h1>
+
+@endsection
